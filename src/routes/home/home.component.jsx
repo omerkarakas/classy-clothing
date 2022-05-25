b6052805a531e9d80086564843489ab0321e9615
Omer Karakas <karakasomer@gmail.com>
2022-05-25 18:04:49 +0300

    navigation menu started

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 import CategoriesList from "../../components/categories-list/categories-list.component";
 
 
 const Home = () => {
-
   return (
-    <CategoriesList />
+    <div>
+      <Outlet />
+      <CategoriesList />
+    </div>
   );
 }
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+
+import { ReactComponent as ClassyLogo } from "../../assets/classy-logo.svg";
+
+import './navigation.styles.scss';
+
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <ClassyLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/">HOME</Link>
+          <Link className="nav-link" to="/shop">SHOP</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
